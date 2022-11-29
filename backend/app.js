984caae721b2bd2bc9c6c2ca8d193f68ba8a7f4a
enimad <aminedjellab92@gmail.com>
2022-11-29 01:35:05 +0100

    Etape 4 presque finalisée pour passage à l'étape 5 > ajouter la fonction like et dislike

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
-
 const app = express();
+
+const path = require('path');
 
 app.use(express.json());
 
@@ -13,8 +14,6 @@ app.use((req, res, next) => {
 
 const mongoose = require('mongoose');
 
-const userRoutes = require('./routes/user');
-
 mongoose.connect('mongodb+srv://enimad:KDc9A1GQEjY1cuKr@cluster0.bkjofor.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true }
@@ -22,32 +21,11 @@ mongoose.connect('mongodb+srv://enimad:KDc9A1GQEjY1cuKr@cluster0.bkjofor.mongodb
 .then(() => console.log('Connexion à MongoDB réussie !'))
 .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.post('/api/sauces', (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json({
-    message: 'Objet créé !'
-  });
-});
+const sauceRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user');
 
-app.get('/api/sauces', (req, res, next) => {
-  const sauces = [
-    {
-      userId: 'oeihfzeoi',
-      name: 'Mon premier objet',
-      manufacturer: 'El loco',
-      description: 'Les infos de mon premier objet',
-      mainPepper: 'Inferno',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      heat: 4,
-      likes: 1,
-      dislikes: 1,
-      usersLiked: [ "String <userId>" ],
-      usersDisliked : [ "String <userId>" ]
-    },
-  ];
-  res.status(200).json(sauces);
-});
-
+app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
---
+++ backend/routes/sauce.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+const sauceCtrl = require('../controllers/sauce');
+
+const router = express.Router();
+
+router.get('/', auth, sauceCtrl.getAllSauce);
+router.post('/', auth, multer, sauceCtrl.createSauce);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
+
+module.exports = router;
